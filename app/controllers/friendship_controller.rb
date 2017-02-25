edf2fd87a84e78f984b0b90648bd503bd7d7a070
lxd <i@lxd.me>
2017-02-25 23:20:34 +0800

    Complete 2 more endpoints

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -19,6 +19,7 @@ class FriendshipController < ApplicationController
     render status: 200
   end
 
+
   def accept_friend_request
     params.require(:from)
     current_user = User.find_by_air_auth_token(request.authorization) # TODO: current_user & `?to=` existence is guaranteed by interceptor
@@ -26,14 +27,26 @@ class FriendshipController < ApplicationController
     friendship = Friendship.find_by(requester_id: params[:from], acceptor_id: current_user.id, status: Friendship.statuses[:pending])
     render json: {message: 'Record not found'}, status: 404 and return if friendship.nil?
 
-    friendship.update(status: Friendship.statuses[:accepted])
-    #TODO Add to array
+    ActiveRecord::Base.transaction do
+      friendship.update(status: Friendship.statuses[:accepted])
+
+      request_user = User.find(params[:from])
+      request_user.friends << current_user.id and request_user.save!
+      current_user.friends << request_user.id and current_user.save!
+    end
   end
+
 
   def get_friend_requests
     current_user = User.find_by_air_auth_token(request.authorization) # TODO: current_user & `?to=` existence is guaranteed by interceptor
 
     ret = Friendship.where(acceptor_id: current_user.id, status: Friendship.statuses[:pending]).pluck(:requester_id)
     render json: ret # TODO: more than ID
+  end
+
+
+  def get_friends
+    current_user = User.find_by_air_auth_token(request.authorization) # TODO: current_user & `?to=` existence is guaranteed by interceptor
+    render json: current_user.friends # TODO: more than ID
   end
 end
